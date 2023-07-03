f499e1a2bd1df2e31a2aa57ebad82b22ba4b8cfc
akuznetsov <akuznetsov@hrsinternational.com>
2023-07-03 20:28:23 +0600

    add new challenges

---
+++ src/main/java/leetcode/RotateString.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+@LeetcodeNumber(number = 796)
+public class RotateString {
+    public static boolean rotateString(String s, String goal) {
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(rotateString("abcde", "cdeab"));
+    }
+}
